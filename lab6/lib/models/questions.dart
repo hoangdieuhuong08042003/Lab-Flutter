c4afa5ba0f9d79459b58f8364d50f5a921dc6adc
huong <hoangdieuhuong08042003@gmail.com>
2025-03-16 14:12:21 +0700

    lab 5- lab 9

---
+++ lab6/lib/models/questions.dart
@@ -0,0 +1,104 @@
+import 'package:lab6/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. Thủ đô của Việt Nam là gì?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Hà Nội',
+      'b) Hồ Chí Minh',
+      'c) Đà Nẵng',
+      'd) Hải Phòng',
+    ],
+  ),
+  Question(
+    question: '2. Ai là người sáng lập ra môn võ Vovinam?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Lý Chiêu Hoàng',
+      'b) Trần Hưng Đạo',
+      'c) Nguyễn Lộc',
+      'd) Hoàng Hoa Thám',
+    ],
+  ),
+  Question(
+    question: '3. Ngày Quốc khánh của Việt Nam là ngày nào?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) 19 tháng 8',
+      'b) 2 tháng 9',
+      'c) 30 tháng 4',
+      'd) 1 tháng 5',
+    ],
+  ),
+  Question(
+    question: '4. Nước nào giáp ranh phía Tây của Việt Nam?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Trung Quốc',
+      'b) Campuchia',
+      'c) Lào',
+      'd) Thái Lan',
+    ],
+  ),
+  Question(
+    question: '5. Tên của ngọn núi cao nhất Việt Nam là gì?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Bạch Mã',
+      'b) Tam Đảo',
+      'c) Ba Vì',
+      'd) Fansipan',
+    ],
+  ),
+  Question(
+    question: '6. Vịnh Hạ Long nằm ở tỉnh nào của Việt Nam?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Quảng Ninh',
+      'b) Thanh Hóa',
+      'c) Nghệ An',
+      'd) Huế',
+    ],
+  ),
+  Question(
+    question: '7. Người Việt Nam thường mặc trang phục gì vào ngày Tết?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Áo dài',
+      'b) Áo the khăn xếp',
+      'c) Áo bà ba',
+      'd) Áo tứ thân',
+    ],
+  ),
+  Question(
+    question: '8. Biển Sầm Sơn thuộc tỉnh nào?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Quảng Bình',
+      'b) Hà Tĩnh',
+      'c) Thanh Hóa',
+      'd) Thừa Thiên Huế',
+    ],
+  ),
+  Question(
+    question: '9. Nguyen Du nổi tiếng với tác phẩm văn học nào?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Lục Vân Tiên',
+      'b) Nhật ký trong tù',
+      'c) Hoàng Lê nhất thống chí',
+      'd) Truyện Kiều',
+    ],
+  ),
+  Question(
+    question: '10. Phở là món ăn truyền thống của vùng nào ở Việt Nam?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Miền Nam',
+      'b) Miền Bắc',
+      'c) Miền Trung',
+      'd) Tây Nguyên',
+    ],
+  ),
+];
